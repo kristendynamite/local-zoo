8ef52ba91a591319bc3599ec93343036d8bb615e
Kristen <kristen.dynamite@gmail.com>
2017-07-07 17:00:54 -0700

    update option drop down selected default

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -7,8 +7,8 @@ import { Animal } from './animal.model';
   <div class="animal-list">
       FILTER BY: <select (change)="onChange($event.target.value)">
         <option value="youngAnimals">Baby Animals</option>
-        <option value="oldAnimals" selected="selected">Animals over 2 Years Old</option>
-        <option value="allAnimals">All Animals</option>
+        <option value="allAnimals" selected="selected">All Animals</option>
+        <option value="oldAnimals">Animals over 2 Years Old</option>
       </select>
       <ul>
         <li (click)="isAge(currentAnimal)" *ngFor="let currentAnimal of childAnimalList | age:filterByAge"> {{currentAnimal.name}} the {{currentAnimal.animal}} <br>Age: {{currentAnimal.age}} <br>Diet: {{currentAnimal.diet}} <br>Location: {{currentAnimal.location}} <br>Caretakers: {{currentAnimal.caretakers}} <br>Gender: {{currentAnimal.sex}} <br>Likes: {{currentAnimal.likes}} <br>Disklikes: {{currentAnimal.dislikes}} <br> {{currentAnimal.image}}
